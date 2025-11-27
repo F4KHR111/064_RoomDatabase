d7dec7df990d7a16117f42368b3d8086c0f04aa8
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-27 15:45:41 +0700

    Memperbaiki AppContainer dan instance Application

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -8,23 +8,22 @@ interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
 }
 
-class ContainerDataApp( val context: Context):
-        ContainerApp {
-            override val repositoriSiswa: RepositoriSiswa by lazy {
-                OfflineRepositoriSiswa(
-                    DatabaseSiswa.getDatabase(context).siswaDao())
-            }
-        }
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
 
-class AplikasiSiswa : Application(){
-
+class AplikasiSiswa : Application() {
     /**
      * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
      */
-    lateinit var containerApp: ContainerApp
+    lateinit var container: ContainerApp
 
     override fun onCreate() {
         super.onCreate()
-        containerApp = ContainerDataApp(this)
+        container = ContainerDataApp(this)
     }
 }

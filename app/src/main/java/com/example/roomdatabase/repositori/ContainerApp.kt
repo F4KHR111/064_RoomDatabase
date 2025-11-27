1f5c94e607fb0d549ad44766bd4796e6792c62a1
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-27 13:22:57 +0700

    Membuat container dan kelas Application untuk injeksi dependensi.

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -1,2 +1,30 @@
 package com.example.roomdatabase.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp( val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao())
+            }
+        }
+
+class AplikasiSiswa : Application(){
+
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var containerApp: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        containerApp = ContainerDataApp(this)
+    }
+}

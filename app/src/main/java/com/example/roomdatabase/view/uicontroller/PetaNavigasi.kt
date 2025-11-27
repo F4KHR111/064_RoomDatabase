807cd0652589550192e1d9fcad49f76fd07f7149
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-27 15:03:46 +0700

    Membuat NavHost dan route untuk HomeScreen

--- app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,7 +4,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabase.view.HomeScreen
+import com.example.roomdatabase.view.route.DestinasiEntry
+import com.example.roomdatabase.view.route.DestinasiHome
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
@@ -17,5 +22,13 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
-
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        
+    }
 }

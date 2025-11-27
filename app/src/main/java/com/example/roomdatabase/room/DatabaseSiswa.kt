ca03572e4532e24eecadee416ea09f84d3d24b12
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-11-27 15:44:58 +0700

    Memperbaiki format kode pada DatabaseSiswa

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -6,22 +6,19 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
-abstract class DatabaseSiswa : RoomDatabase() {
-    abstract fun siswaDao(): SiswaDao
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
 
     companion object {
         @Volatile
         private var Instance: DatabaseSiswa? = null
-        fun getDatabase(context: Context): DatabaseSiswa {
-            return (Instance ?: synchronized(this) {
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return  (Instance?: synchronized(this){
                 Room.databaseBuilder(
-                    context, klass = DatabaseSiswa::class.java,
-                    "siswa_database"
-                )
-                    .build().also { Instance = it }
-
+                    context, DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also { Instance=it }
             })
-
         }
     }
 }

1ee7fa299ffef038e89185a5ab894910dcb15052
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-12-04 11:58:52 +0700

    Membuat companion object untuk TIMEOUT_MILLIS

--- app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
@@ -30,3 +30,7 @@ class DetailViewModel (
     suspend fun deleteSiswa(){
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
